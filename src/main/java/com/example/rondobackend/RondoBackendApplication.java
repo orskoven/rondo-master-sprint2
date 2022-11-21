c7c9c9640acb4d39d62b6c4c70cc2685195d7e68
rasmuswoldike <rswoldike@gmail.com>
2022-11-21 14:24:20 +0100

    Initial commit

---
+++ src/main/java/com/example/rondobackend/RondoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rondobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RondoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RondoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rondobackend/RondoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rondobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RondoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
